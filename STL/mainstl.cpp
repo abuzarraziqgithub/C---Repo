97c0b8c6f3462e6f5fc1900fb56fbf9757edb0e5
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-12-01 10:00:04 +0500

    pairs explained

---
+++ STL/mainstl.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<string, int> p = {"hello", 2};
+    cout<< p.first << endl;
+
+    int arr[] = {1,2,3,4,5,6,7,8,9,5,2,3,3};
+    cout << arr[5] << endl;
+
+    pair<string, int> arr2[] = { {"abuzar", 4}, {"he",3} };
+
+}
+
