8ddefe6a264420b3a7862c74ce1bf14f848335f1
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-12-01 10:21:31 +0500

    vectors explained

--- STL/mainstl.cpp
+++ STL/mainstl.cpp
@@ -3,13 +3,27 @@
 using namespace std;
 
 int main(){
-    pair<string, int> p = {"hello", 2};
-    cout<< p.first << endl;
+    // Arrays are contsant and we can't modify it
+    // Vectors are dynamic and we can add, remove elements
 
-    int arr[] = {1,2,3,4,5,6,7,8,9,5,2,3,3};
-    cout << arr[5] << endl;
+    vector<int> v;
+    v.push_back(1);
+    v.emplace_back(2);
 
-    pair<string, int> arr2[] = { {"abuzar", 4}, {"he",3} };
+    // Using pair
+    vector<pair<int, int>> p;
+    p.push_back({1,2});
+    // syntax is changed for emplace_back(it is also faster than push_back)
+    p.emplace_back(3,4);
+
+    vector<int> s(5, 100); // {100,100,100,100,100}
+    vector<int> s2(5); // {0,0,0,0,0} or any garbage instances depends on the compiler and despite of giving the size we can push,empace value to it so it will dynamically allocate it on the back
+
+    vector<int> s3(5,20);
+    vector<int> s4(s3);// pass by value so it will copy the s3
+    // we can access the elements what we do using arrays and v.at() but v[] is preffered
+
+
 
 }
 
